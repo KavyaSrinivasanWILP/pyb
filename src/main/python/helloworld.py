89798aa730a0678b7a591ace1beaa7fb14ed6437
KavyaSrinivasanWILP <161742042+KavyaSrinivasanWILP@users.noreply.github.com>
2024-03-06 08:52:53 +0530

    Update helloworld.py with docstring

--- src/main/python/helloworld.py
+++ src/main/python/helloworld.py
@@ -1,4 +1,10 @@
 import sys
 
 def helloworld(out):
+    """
+    Prints 'Hello world of Python' to the provided output stream.
+    
+    Parameters:
+        out (file-like object): The output stream to write the message to.
+    """
     out.write("Hello world of Python\n")
